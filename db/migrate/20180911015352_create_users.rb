abc80cfb099c610f95906cf9f0953a9760855c36
Matt Van Dyke <matthewlvandyke@gmail.com>
2018-09-11 23:06:38 -0400

    Fix Heroku bug

--- db/migrate/20180911015352_create_users.rb
+++ db/migrate/20180911015352_create_users.rb
@@ -1,6 +1,5 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    drop_table :users
     create_table :users do |t|
       t.string :username, null: false
       t.string :email, null: false
