a4920c6816312813a5eca428d9c08f6dff96a37f
Matt Van Dyke <matthewlvandyke@gmail.com>
2018-09-14 13:56:58 -0400

    Style menu dropdown

--- frontend/components/client/menu/menu_user_dropdown.jsx
+++ frontend/components/client/menu/menu_user_dropdown.jsx
@@ -4,16 +4,21 @@ const UserDropdown = ({currentUser, logout}) => {
   return (
     <div className="user-dropdown-container">
       <div className="user-account">
-        <div className="container-username">
-          {currentUser.username}<i>v</i>
+        <div className="username-container">
+          <span className="initial">{currentUser.username[0].toUpperCase()}</span>
+          <span className="user">{currentUser.username}</span>
+          <img
+            src={window.dropArrow}/>
         </div>
-        <ul className="dropdown">
-          <li className="dropdown-account">Account</li>
-          <li className="dropdown-username"></li>
-          <li onClick={() => logout()} className="dropdown-item">
-            Sign out {currentUser.username}
-          </li>
-        </ul>
+        <div className="dropdown">
+          <div className="dropdown-account">Account</div>
+          <ul>
+            <li className="dropdown-username"></li>
+            <li onClick={() => logout()} className="dropdown-item">
+              Sign out {currentUser.username}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
