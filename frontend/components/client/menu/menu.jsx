a72c0ab2382c1ffa6718012df4de0cca17e3e5e9
Matt Van Dyke <matthewlvandyke@gmail.com>
2018-09-15 10:57:28 -0400

    Powerup create note circle icon

--- frontend/components/client/menu/menu.jsx
+++ frontend/components/client/menu/menu.jsx
@@ -25,7 +25,9 @@ class Menu extends React.Component {
       <aside className="menu-container">
         <div className="user-account">
           <div onClick={() => this.displayDropdown()} className="username-container">
-              <span className="initial">{this.props.currentUser.username[0].toUpperCase()}</span>
+              <span className="initial-container">
+                <span className="initial">{this.props.currentUser.username[0].toUpperCase()}</span>
+              </span>
               <span className="user">{this.props.currentUser.username}</span>
             <img src={window.dropArrow}/>
           </div>
@@ -37,13 +39,26 @@ class Menu extends React.Component {
               logout={this.props.logout} /> : null
         }
         <div className="create-note" onClick={() => createNote(defaultNote)}>
-          <span className="create-plus-container">
-            <span className="create-plus">+</span>
-          </span> New Note
+
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" id="qa-CREATE_NOTE">
+            <g fill="none" fillRule="evenodd">
+              <path d="M0 0h30v30H0z"></path>
+              <circle cx="15" cy="15" r="14" fill="#00A82D"></circle>
+              <rect width="14" height="2" x="8" y="14" fill="#FFF" rx="1"></rect>
+              <rect width="2" height="14" x="14" y="8" fill="#FFF" rx="1"></rect>
+            </g>
+          </svg>
+
+          New Note
         </div>
-        <NavLink
-          to="/client/notebooks"
-          activeClassName="selected-link">Notebooks</NavLink>
+        <div className="nav-links">
+          <NavLink
+            to="/client/notes"
+            activeClassName="selected-link">All Notes</NavLink>
+          <NavLink
+            to="/client/notebooks"
+            activeClassName="selected-link">Notebooks</NavLink>
+        </div>
       </aside>
     )
   }
